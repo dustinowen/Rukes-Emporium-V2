f241674373336d7f0a82fb81e22dbe57d59ff720
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-10-19 22:31:06 -0700

    Compelte item category components

--- src/App.js
+++ src/App.js
@@ -1,48 +1,40 @@
-import './category.styles.scss'
+import CategoryItem from "./components/category-item/category-item.component";
+import './categories-styles.scss'
 
 function App() {
-
   const categories = [
     {
       id: 1,
-      title: 'Grub',
-      imageUrl: 'https://i.imgur.com/kNfKmSN.png'
+      title: "Grub",
+      imageUrl: "https://i.imgur.com/kNfKmSN.png",
     },
     {
       id: 2,
-      title: 'Nibbles',
-      imageUrl: 'https://i.imgur.com/4dv4XI2.png'
+      title: "Nibbles",
+      imageUrl: "https://i.imgur.com/4dv4XI2.png",
     },
     {
       id: 3,
-      title: 'Supplies',
-      imageUrl: 'https://i.imgur.com/wOhv2E0.png'
+      title: "Supplies",
+      imageUrl: "https://i.imgur.com/wOhv2E0.png",
     },
     {
       id: 4,
-      title: 'Playthings',
-      imageUrl: 'https://i.imgur.com/pR9ijCc.png'
+      title: "Playthings",
+      imageUrl: "https://i.imgur.com/pR9ijCc.png",
     },
     {
       id: 5,
-      title: 'Threads',
-      imageUrl: 'https://i.imgur.com/J2sPwmP.png'
+      title: "Threads",
+      imageUrl: "https://i.imgur.com/J2sPwmP.png",
     },
-  ]
+  ];
 
   return (
-    <div className='categories-container'>
-
-      {categories.map(({ title, id, imageUrl }) => (
-        <div key={id} className='category-container'>
-          <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
-            <div className='category-body-container'>
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-  ))}
-
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
   );
 }
