855b1da3ce636674f5627abadf42d865080959ac
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-10-20 16:10:18 -0700

    Create home and navbar component, begin work on routing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-import CategoryMenu from "./components/category-menu/category-menu.component";
+import { Routes, Route } from "react-router-dom";
+
+import NavBar from "./routes/navbar/navbar.component";
+import Home from "./routes/home/home.component";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+
 
 
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "grub",
-      imageUrl: "https://i.imgur.com/kNfKmSN.png",
-    },
-    {
-      id: 2,
-      title: "nibbles",
-      imageUrl: "https://i.imgur.com/pAn4ybw.png",
-    },
-    {
-      id: 3,
-      title: "supplies",
-      imageUrl: "https://i.imgur.com/9Cut1Pn.png",
-    },
-    {
-      id: 4,
-      title: "playthings",
-      imageUrl: "https://i.imgur.com/xxNN7Gd.png",
-    },
-    {
-      id: 5,
-      title: "threads",
-      imageUrl: "https://i.imgur.com/J2sPwmP.png",
-    },
-  ];
-
   return (
-    <CategoryMenu categories={categories} />
+    <Routes>
+      <Route path="/" element={<NavBar />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,42 @@
+import { Outlet } from "react-router-dom";
+
+import CategoryMenu from "../../components/category-menu/category-menu.component";
+
+function Home() {
+  const categories = [
+    {
+      id: 1,
+      title: "grub",
+      imageUrl: "https://i.imgur.com/kNfKmSN.png",
+    },
+    {
+      id: 2,
+      title: "nibbles",
+      imageUrl: "https://i.imgur.com/pAn4ybw.png",
+    },
+    {
+      id: 3,
+      title: "supplies",
+      imageUrl: "https://i.imgur.com/9Cut1Pn.png",
+    },
+    {
+      id: 4,
+      title: "playthings",
+      imageUrl: "https://i.imgur.com/xxNN7Gd.png",
+    },
+    {
+      id: 5,
+      title: "threads",
+      imageUrl: "https://i.imgur.com/J2sPwmP.png",
+    },
+  ];
+
+    return (
+      <div>
+            <Outlet />
+            <CategoryMenu categories={categories} />
+      </div>
+  );
+}
+
+export default Home;
