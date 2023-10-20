2a26903441592291491749d4e298c186c864c32d
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-10-20 11:27:26 -0700

    Build category-menu components - clean up app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
-import CategoryItem from "./components/category-item/category-item.component";
-import './categories-styles.scss'
+import CategoryMenu from "./components/category-menu/category-menu.component";
+
 
 function App() {
   const categories = [
     {
       id: 1,
-      title: "Grub",
+      title: "grub",
       imageUrl: "https://i.imgur.com/kNfKmSN.png",
     },
     {
       id: 2,
-      title: "Nibbles",
-      imageUrl: "https://i.imgur.com/4dv4XI2.png",
+      title: "nibbles",
+      imageUrl: "https://i.imgur.com/pAn4ybw.png",
     },
     {
       id: 3,
-      title: "Supplies",
-      imageUrl: "https://i.imgur.com/wOhv2E0.png",
+      title: "supplies",
+      imageUrl: "https://i.imgur.com/9Cut1Pn.png",
     },
     {
       id: 4,
-      title: "Playthings",
-      imageUrl: "https://i.imgur.com/pR9ijCc.png",
+      title: "playthings",
+      imageUrl: "https://i.imgur.com/xxNN7Gd.png",
     },
     {
       id: 5,
-      title: "Threads",
+      title: "threads",
       imageUrl: "https://i.imgur.com/J2sPwmP.png",
     },
   ];
 
   return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
+    <CategoryMenu categories={categories} />
   );
 }
 
