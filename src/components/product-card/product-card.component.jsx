49efed29d9652b4fbb7b7dbcd82ad1125f588f24
dustinowen <137654369+dustinowen@users.noreply.github.com>
2023-11-02 16:30:29 -0700

    Further develop cart context, enable adding items to cart, enable quantity of items in cart

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component'
 import './product-card.styles.scss'
 
 const ProductCard = ({ product }) => {
 
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext)
+
+    const addProductToCart = () => addItemToCart(product)
+        
         
     return (<div className='product-card-container'>
 
@@ -14,7 +20,7 @@ const ProductCard = ({ product }) => {
             <span className='price'>{price}</span>
         </div>
 
-        <Button buttonType='inverted'>Add To Cart</Button>
+        <Button buttonType='inverted' onClick={addProductToCart}>Add To Cart</Button>
 
     </div>)
 }
